6fa7cb6f6cc234f4e8212c1a9f331abaad0c8ee4
wiztemple <wiztemple@gmail.com>
2019-08-05 08:34:44 +0100

    add moving emojis

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@
  */
 const accordions = document.querySelectorAll('.accordion');
 const toggle = () => {
-  // const accordionContent = document.getElementsByTagName('accordion-content');
   accordions.forEach(accordion => {
     const accordionHeader = accordion.querySelector('.accordion-header');
     accordionHeader.addEventListener('click', () => {
@@ -13,3 +12,43 @@ const toggle = () => {
 }
 toggle();
 
+/**
+ * function to toggle a custom select
+ */
+// const selectItems = document.querySelectorAll('.select-item');
+// const toggleSelect = () => {
+//   selectItems.forEach(selectItem => {
+//     const inputField = selectItem.querySelector('.input-field');
+//     inputField.addEventListener('click', () => {
+//       selectItem.classList.toggle('active');
+//     }, false)
+//   })
+// }
+// toggleSelect();
+
+const selectItems = document.querySelectorAll('.flames-select');
+const toggleSelect = () => {
+  selectItems.forEach(selectItem => {
+    const inputField = selectItem.querySelector('.input-field');
+    const icon = selectItem.querySelector('.icon-right');
+    const ul = selectItem.querySelector('.select-item');
+    inputField.addEventListener('click', () => {
+      ul.classList.toggle('active');
+      icon.classList.toggle('active');
+    })
+  })
+}
+toggleSelect();
+
+
+const alertInfo = Array.from(document.getElementsByClassName('alert-info'))[0];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const alert = document.getElementById('pos-absolute');
+  alert.style.display = 'block';
+
+  setTimeout(() => {
+    alert.classList.replace('slideIn', 'fadeOut');
+    alert.style.display = 'none';
+  }, 4000)
+});
